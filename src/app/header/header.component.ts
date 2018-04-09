a3c2f893cf3ed985fb6ef1a905bdb173db570430
Ouraborus <1222eca@gmail.com>
2018-04-09 01:07:49 -0500

    Working with sprites, not finished

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+//ALERTA!!!! MACHETE INCOMING
+declare var require: any
+// ↑ BORRAR ESA GUEVONADA
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  imageSrc = require('../../images/pokemons-sac4445def4.png');
 
   ngOnInit() {
   }
